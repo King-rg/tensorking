04b640b6acead79ab405db0e7b22a9b402183571
David King <david@centran.us>
2022-01-21 15:46:08 -0500

    Moving updates to other computer

--- tensorking/models.py
+++ tensorking/models.py
@@ -1,15 +1,25 @@
-class Sequential:
-    # List containing layer class'
-    def __init__(self):
-        self.layers = []
+import numpy as np
 
+class Sequential():
+    # List containing layer class'
+    def __init__(self, layers: list, input_shape):
+        self.layers = layers
+        self.input_shape = input_shape
+
+    def link(self):
+        x=0
+        while (x < len(self.layers)):
+            self.layers[x]
+            
+            x=x+1
 
     def forward(self, inputs):
-        
-        for layer in self.layers:
-            inputs = layer.forward(inputs)
+        output = 0
 
-        return inputs 
+        for layer in self.layers:
+            
+
+        return output 
 
     # Fitting function
     def fit(self):
