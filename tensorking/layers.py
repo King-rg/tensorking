ac2ffed792295fedfe36cc2b5080256e5a715dfc
King-rg <david@centran.us>
2022-01-22 14:32:08 -0500

    Refactor preparation function

--- tensorking/layers.py
+++ tensorking/layers.py
@@ -10,12 +10,10 @@ class Dense:
         self.biases = np.zeros((1, n_neurons))
         
         self.next_layer = False
-
-
+ 
     # Create the weights necessary for the forward function
     def activate(self, n_inputs):
         self.weights = 0.01 * np.random.randn(n_inputs,self.n_neurons)
-
 
     # Forward pass
     def forward(self, inputs):
