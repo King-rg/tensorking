a3ec4fd60eae2c411bcd4b8b10c05db4d9d35a79
David King <david@centran.us>
2022-01-21 20:01:19 -0500

    Making network wide calculations automated

--- tensorking/models.py
+++ tensorking/models.py
@@ -1,25 +1,34 @@
 import numpy as np
 
 class Sequential():
+    """
+    1. Get input data
+    2. Check number of inputs
+    3. Begin network preparation
+    4. Run layer activation
+    """
+    
     # List containing layer class'
-    def __init__(self, layers: list, input_shape):
+    def __init__(self, layers: list):
         self.layers = layers
-        self.input_shape = input_shape
 
-    def link(self):
+    def prepare(self, inputs):
         x=0
         while (x < len(self.layers)):
-            self.layers[x]
-            
+            if (x == 0):
+                self.layers[x].activate(inputs.shape[len(inputs.shape)-1])
+                if (x < len(self.layers)-1):
+                    self.layers[x].next_layer = self.layers[x+1]
+            else:
+                self.layers[x].activate(self.layers[x-1].n_neurons)
+                if (x < len(self.layers)-1):
+                        self.layers[x].next_layer = self.layers[x+1]
             x=x+1
 
     def forward(self, inputs):
-        output = 0
-
-        for layer in self.layers:
-            
-
-        return output 
+        self.prepare(inputs)
+        
+        return self.layers[0].forward(inputs)
 
     # Fitting function
     def fit(self):
